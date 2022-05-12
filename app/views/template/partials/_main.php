4b19b428ac5fba4f68b8cac5f1a4ddca7a7cbf02
Yann <yannickdemonthy@hotmail.be>
2022-05-12 13:37:18 +0200

    Découpage du template

---
+++ app/views/template/partials/_main.php
@@ -0,0 +1,14 @@
+<section class="ftco-section ftco-degree-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 ftco-animate">
+                <div class="container">
+                    <?php echo $content; ?>
+                </div>
+            </div>
+
+            <?php include '../app/views/template/partials/_asside.php'; ?>
+
+        </div>
+    </div>
+</section>
